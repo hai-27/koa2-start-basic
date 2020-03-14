faaf7f56c1e65f9c6017ba8042ec1ece36cb48fd
hai-27 <2601615158@qq.com>
2020-03-14 23:08:31 +0800

    Using koa-body middleware to process request body data.

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
  * @Author: hai-27
  * @Date: 2020-03-14 20:14:45
  * @LastEditors: hai-27
- * @LastEditTime: 2020-03-14 22:52:18
+ * @LastEditTime: 2020-03-14 23:01:44
  */
 const Koa = require('koa');
 const KoaStatic = require('koa-static');
+const KoaBody = require('koa-body');
 
-let { Port, staticDir } = require('./config');
+let { Port, staticDir, uploadDir } = require('./config');
 
 let app = new Koa();
 
@@ -33,6 +34,22 @@ app.use(async (ctx, next) => {
 });
 // 使用koa-static处理静态资源
 app.use(KoaStatic(staticDir));
+
+// 处理请求体数据
+app.use(KoaBody({
+  multipart: true,
+  // parsedMethods默认是['POST', 'PUT', 'PATCH']
+  parsedMethods: ['POST', 'PUT', 'PATCH', 'GET', 'HEAD', 'DELETE'],
+  formidable: {
+    uploadDir: uploadDir, // 设置文件上传目录
+    keepExtensions: true, // 保持文件的后缀
+    maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小限制
+    onFileBegin: (name, file) => { // 文件上传前的设置
+      // console.log(`name: ${name}`);
+      // console.log(file);
+    }
+  }
+}));
 
 // 使用路由中间件
 const Routers = require('./src/routers');
--- config.js
+++ config.js
@@ -3,13 +3,14 @@
  * @Author: hai-27
  * @Date: 2020-03-14 20:27:52
  * @LastEditors: hai-27
- * @LastEditTime: 2020-03-14 22:51:29
+ * @LastEditTime: 2020-03-14 23:01:32
  */
 const path = require('path');
 
 module.exports = {
   Port: 5000, // 启动端口
   staticDir: path.resolve('./public'), // 静态资源路径
+  uploadDir: path.join(__dirname, path.resolve('public/')), // 上传文件路径
   // 数据库连接设置
   dbConfig: {
     connectionLimit: 10,
