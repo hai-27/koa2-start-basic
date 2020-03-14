f41ee9b01c3d77631ff99f60acc8ade388718eb7
hai-27 <2601615158@qq.com>
2020-03-14 22:46:21 +0800

    Unified processing of error messages.

--- app.js
+++ app.js
@@ -2,13 +2,26 @@
  * @Author: hai-27
  * @Date: 2020-03-14 20:14:45
  * @LastEditors: hai-27
- * @LastEditTime: 2020-03-14 21:35:35
+ * @LastEditTime: 2020-03-14 22:44:48
  */
 const Koa = require('koa');
 
 let { Port } = require('./config');
 
 let app = new Koa();
+
+// 统一处理异常
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (error) {
+    console.log(error);
+    ctx.body = {
+      code: '500',
+      msg: '服务器未知错误'
+    }
+  }
+});
 
 // 使用路由中间件
 const Routers = require('./src/routers');
