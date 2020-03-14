a8ccfa90ccac9fbd71ca2cd877585063423b8ff5
hai-27 <2601615158@qq.com>
2020-03-14 22:29:53 +0800

    Create a database connection object.

--- config.js
+++ config.js
@@ -3,8 +3,17 @@
  * @Author: hai-27
  * @Date: 2020-03-14 20:27:52
  * @LastEditors: hai-27
- * @LastEditTime: 2020-03-14 20:30:07
+ * @LastEditTime: 2020-03-14 22:16:38
  */
 module.exports = {
   Port: 5000, // 启动端口
+
+  // 数据库连接设置
+  dbConfig: {
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'storeDB'
+  }
 }
---
+++ src/models/db.js
@@ -0,0 +1,40 @@
+/*
+ * @Description: 
+ * @Author: hai-27
+ * @Date: 2020-03-14 22:21:48
+ * @LastEditors: hai-27
+ * @LastEditTime: 2020-03-14 22:23:09
+ */
+var mysql = require('mysql');
+const { dbConfig } = require('../../config.js');
+var pool = mysql.createPool(dbConfig);
+
+var db = {};
+
+db.query = function (sql, params) {
+
+  return new Promise((resolve, reject) => {
+    // 取出链接
+    pool.getConnection(function (err, connection) {
+
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      connection.query(sql, params, function (error, results, fields) {
+        console.log(`${ sql }=>${ params }`);
+        // 释放连接
+        connection.release();
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(results);
+      });
+
+    });
+  });
+}
+// 导出对象
+module.exports = db;
